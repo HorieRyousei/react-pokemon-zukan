75dc2ebf3a9b01bd332e01927f1f39395f07f2aa
HorieRyousei <ktc24a31e0013@edu.kyoto-tech.ac.jp>
2024-10-12 15:48:36 +0900

    a

--- src/queryKeys.ts
+++ src/queryKeys.ts
@@ -1,5 +1,5 @@
 // src/queryKeys.ts
-import { createQueryKeys, mergeQueryKeys } from "@lukemorales/query-key-factory";
+import { createQueryKeys, mergeQueryKeys } from "@lukemorales/query-key-factory"
 
 export const pokemonQueryKeys = createQueryKeys("pokemon", {
     list: () => ["list"],
@@ -7,3 +7,5 @@ export const pokemonQueryKeys = createQueryKeys("pokemon", {
 });
 
 export const apiQueryKeys = mergeQueryKeys(pokemonQueryKeys);
+
+
